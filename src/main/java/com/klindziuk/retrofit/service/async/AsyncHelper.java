9b42c370a2c90864f8235fbb771510ef141c92a6
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-07-26 17:06:12 +0300

    sync + async(resolved) +helper

--- src/main/java/com/klindziuk/retrofit/service/async/AsyncHelper.java
+++ src/main/java/com/klindziuk/retrofit/service/async/AsyncHelper.java
@@ -2,16 +2,23 @@ package com.klindziuk.retrofit.service.async;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+
 public final class AsyncHelper {
+	private static final Logger logger = Logger.getLogger(AsyncHelper.class);
+	private static final String ERROR_MESSAGE = "Thread was interrupted";
+	private static final int FREEZETIME = 6;
+	
 	private AsyncHelper() {}
 	
 	public static void waitForResponse(ASyncServiceManager manager) {
 		Thread t = new Thread(manager);
 		t.start();
 		try {
-		    TimeUnit.SECONDS.sleep(6);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
+		    TimeUnit.SECONDS.sleep(FREEZETIME);
+		} catch (InterruptedException inex) {
+		logger.error(ERROR_MESSAGE + Thread.currentThread().getName(), inex);	
+		  
 		}
 	}
 }
